8ec62f8243b6b26cb730421a3c3d154f4f4aa4d6
Karl Penzhorn <karlpenzhorn@gmail.com>
2015-04-14 13:24:46 +0200

    menu state button to play state ignore exports

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -7,19 +7,33 @@ import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.util.FlxMath;
 
+using flixel.util.FlxSpriteUtil;
+import flixel.util.FlxDestroyUtil;
+
 /**
  * A FlxState which can be used for the game's menu.
  */
 class MenuState extends FlxState
 {
+	private var _btnPlay:FlxButton;
+
 	/**
 	 * Function that is called up when to state is created to set it up. 
 	 */
 	override public function create():Void
 	{
+		_btnPlay = new FlxButton(0,0,"Play",clickPlay);
+		add(_btnPlay);
+		_btnPlay.screenCenter();
+
 		super.create();
 	}
 	
+	private function clickPlay():Void
+	{
+		FlxG.switchState(new PlayState());
+	}
+
 	/**
 	 * Function that is called when this state is destroyed - you might want to 
 	 * consider setting all objects this state uses to null to help garbage collection.
@@ -27,6 +41,8 @@ class MenuState extends FlxState
 	override public function destroy():Void
 	{
 		super.destroy();
+
+		_btnPlay = FlxDestroyUtil.destroy(_btnPlay);
 	}
 
 	/**
